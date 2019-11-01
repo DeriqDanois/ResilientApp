8765a805858e00e9d9a5886f0bea8bc30354053c
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-10-31 21:19:40 -0700

    linked fonts and assets and worked on the emergency page

--- styles/NavBarStyles.js
+++ styles/NavBarStyles.js
@@ -5,18 +5,25 @@ const NavBarStyles = StyleSheet.create({
         textAlign: "center",
         width: "100%",
         height: 75,
-        backgroundColor: 'white',
+        backgroundColor: '#E7F7FC',
         borderWidth: 0.5,
-        borderColor: '#568C9E',
+        borderColor: '#ECEAED',
         flexDirection: 'row',
         borderTopLeftRadius: 40,
         borderTopRightRadius: 40,
-        shadowColor: 'black',
+        // shadowColor: 'black',
+        // shadowOpacity: 1,
+        // elevation: 20,
+        // shadowRadius: 50 ,
+        // shadowOffset : { width: 56, height: 23},
+        shadowColor: "black",
+        shadowOffset: {
+            width: 2,
+            height: 5,
+        },
         shadowOpacity: 1,
-        elevation: 20,
-        shadowRadius: 50 ,
-        shadowOffset : { width: 56, height: 23},    
-  
+        shadowRadius: 3,
+        elevation: 6
     },
 
     icons: {
@@ -41,9 +48,13 @@ const NavBarStyles = StyleSheet.create({
     },
     
     sosText: {
-        fontSize: 16,
+        fontSize: 20,
         color: 'white',
         fontWeight: 'bold',
+        fontFamily:'NunitoSans-ExtraBold',
+        textShadowColor: 'black',
+       textShadowOffset: { width: 0.5, height: 1 },
+       textShadowRadius: 5
     },
     Image: {
         width:45,
