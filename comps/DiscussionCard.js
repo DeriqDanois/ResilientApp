2d9443cebbfd1c9f4199e70a7e021fc05d349650
tashavithaya <tashavithaya@gmail.com>
2019-11-15 15:21:42 -0800

    Added PNGs, IMGs in Assets / DiscussionCard comp and styles

---
+++ comps/DiscussionCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+import { View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    SafeAreaView,
+    ScrollView,
+    Button } from 'react-native';
+//import DiscussionStyles from '../styles/PageStyles/DiscussionStyles';
+
+//make discussion card with text, img, icon
+//style the comps
+
+//populate the discussion page with cards
---
+++ styles/ComponentStyles/DiscussionCardStyles.js
@@ -0,0 +1,20 @@
+import { Platform, StyleSheet } from 'react-native';
+
+
+// const styles = Stylesheet.create({
+//   button: {
+
+//     // cross-platform css
+
+//     ...Platform.select({
+//       ios: {
+//         shadowColor: rgba(0,0,0),
+//         shadowOpacity: 0.5,
+//         shadowRadius: 5
+//       },
+//       android: {
+//         elevation: 5
+//       },
+//     }),
+//   }
+// })
